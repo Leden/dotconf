c81afa61408286ec924ad014831a08d09d2a3586
Denis Lesnov <denis@lesnov.me>
2019-07-18 21:33:01 +0300

    upd

--- cfg/custom/asdf/install.sh
+++ cfg/custom/asdf/install.sh
@@ -2,8 +2,13 @@
 
 set -xe
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.3
+[ -d $HOME/.asdf ] || git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.7.3
 
 mkdir -p $HOME/cfg/bash/include/
-ln -s $HOME/cfg/custom/asdf/bashrc $HOME/cfg/bash/include/asdf
+[ -L $HOME/cfg/bash/include/asdf ] || ln -s $HOME/cfg/custom/asdf/bashrc $HOME/cfg/bash/include/asdf
+
+sudo apt install automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev \
+  unzip curl
 
