c64835fe3a36c22486505220caef572814511c18
Denis Lesnov <denis@lesnov.me>
2019-07-29 23:27:35 +0300

    Vintage Story game install script

---
+++ cfg/games/vintagestory/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Depends :: mono
+
+set -xe
+
+VERSION="1.9.14"
+FILENAME="vs_archive_${VERSION}.tar.gz"
+DOWNLOAD_URL="https://cdn.vintagestory.at/gamefiles/stable/$FILENAME"
+TMP_DIR=$(mktemp -d)
+
+cd "$TMP_DIR"
+wget "$DOWNLOAD_URL" -O "$TMP_DIR/$FILENAME"
+tar xzvf "$TMP_DIR/$FILENAME"
+
+# Required for installer to create a menu entry and a desktop icon
+mkdir -p "$HOME/.local/share/applications/"
+mkdir -p "$(xdg-user-dir DESKTOP)/"
+
+"$TMP_DIR/vintagestory/install.sh"
+
+cd "$HOME"
+rm -rf "$TMP_DIR"
