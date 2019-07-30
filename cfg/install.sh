0b59a06747178fc5279e001e46ca6d353ccc65fa
Denis Lesnov <denis@lesnov.me>
2019-07-30 14:37:42 +0300

    install custom repos from install script

--- cfg/install.sh
+++ cfg/install.sh
@@ -10,4 +10,7 @@ ensure_line_in_file () {
 ensure_line_in_file '. $HOME/cfg/bash/bashrc #:cfg:bash:bashrc' "$HOME/.bashrc"
 ensure_line_in_file 'source $HOME/cfg/vim/vimrc ":cfg:vim:vimrc' "$HOME/.vimrc"
 
-
+for setup_repo in "$HOME"/cfg/custom/*/repo.sh
+do
+	$setup_repo
+done
