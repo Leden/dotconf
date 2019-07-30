925b0c723f3ecf5ccdc77e1debbc11a84c996c75
Denis Lesnov <denis@lesnov.me>
2019-07-30 15:20:38 +0300

    bootstrap checkout ignores local changes

--- cfg/bootstrap.sh
+++ cfg/bootstrap.sh
@@ -9,7 +9,7 @@ rm -rf "$HOME/.cfg"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 git clone --bare "https://github.com/Leden/dotconf.git" "$HOME/.cfg"
-config checkout
+config checkout --force
 config config --local status.showUntrackedFiles no
 
 "$HOME/cfg/install.sh"
