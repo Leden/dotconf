76ffab1d08308ee8095cea20c02c006f0e7708b8
Denis Lesnov <denis@lesnov.me>
2019-07-30 14:48:43 +0300

    bootstrap with https as the host probably does not yet have an ssh key set up

--- cfg/bootstrap.sh
+++ cfg/bootstrap.sh
@@ -5,6 +5,6 @@ set -ex
 sudo apt update
 sudo apt install -y git
 
-git clone --bare git@github.com:Leden/dotconf.git "$HOME/.cfg"
+git clone --bare "https://github.com/Leden/dotconf.git" "$HOME/.cfg"
 
 "$HOME/cfg/install.sh"
