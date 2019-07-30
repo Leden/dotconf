59a89199451d29a7e2123d793eb421172dbb82d2
Denis Lesnov <denis@lesnov.me>
2019-07-30 14:58:06 +0300

    upd

--- cfg/bootstrap.sh
+++ cfg/bootstrap.sh
@@ -5,6 +5,10 @@ set -ex
 sudo apt update
 sudo apt install -y git
 
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
 git clone --bare "https://github.com/Leden/dotconf.git" "$HOME/.cfg"
+config checkout
+config config --local status.showUntrackedFiles no
 
 "$HOME/cfg/install.sh"
