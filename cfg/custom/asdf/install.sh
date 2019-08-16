f6a7a77884a7d0bbd0fcc88c21e093b7f5053da6
Denis Lesnov <denis@lesnov.me>
2019-08-16 11:56:42 +0300

    asdf install script fix

--- cfg/custom/asdf/install.sh
+++ cfg/custom/asdf/install.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-[ -d $HOME/.asdf ] || git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.7.3
+[ -d "$HOME/.asdf" ] || git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.7.3
 
-mkdir -p $HOME/cfg/bash/include/
-[ -L $HOME/cfg/bash/include/asdf ] || ln -s $HOME/cfg/custom/asdf/bashrc $HOME/cfg/bash/include/asdf
+mkdir -p "$HOME/cfg/bash/include/"
+[ -L "$HOME/cfg/bash/include/asdf" ] || ln -s "$HOME/cfg/custom/asdf/bashrc" "$HOME/cfg/bash/include/asdf"
 
-sudo apt install automake autoconf libreadline-dev \
+sudo apt install -y automake autoconf libreadline-dev \
   libncurses-dev libssl-dev libyaml-dev \
   libxslt-dev libffi-dev libtool unixodbc-dev \
   unzip curl
