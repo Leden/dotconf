d064ac2fdd4dd6b825a2c5ee92ac2e7f4f1840b0
Denis Lesnov <denis@lesnov.me>
2019-08-16 12:01:06 +0300

    try bootstrap via ssh if possible, fallback to https

--- cfg/bootstrap.sh
+++ cfg/bootstrap.sh
@@ -2,13 +2,16 @@
 
 set -ex
 
+repo_url_https="https://github.com/Leden/dotconf.git"
+repo_url_ssh="git@github.com:Leden/dotconf.git"
+
 sudo apt update
 sudo apt install -y curl git
 
 rm -rf "$HOME/.cfg"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-git clone --bare "https://github.com/Leden/dotconf.git" "$HOME/.cfg"
+git clone --bare "$repo_url_ssh" "$HOME/.cfg" || git clone --bare "$repo_url_https" "$HOME/.cfg"
 config checkout --force
 config config --local status.showUntrackedFiles no
 
