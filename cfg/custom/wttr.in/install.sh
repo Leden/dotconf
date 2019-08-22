62c6c71de207d0a59bd3e184d2225e1f459f61a6
Denis Lesnov <denis@lesnov.me>
2019-08-22 23:03:00 +0300

    wttr install shellcheck

--- cfg/custom/wttr.in/install.sh
+++ cfg/custom/wttr.in/install.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-mkdir -p $HOME/cfg/bash/include/
-[ -L $HOME/cfg/bash/include/wttr.in ] || ln -s $HOME/cfg/custom/wttr.in/bashrc $HOME/cfg/bash/include/wttr.in
+mkdir -p "$HOME/cfg/bash/include/"
+[ -L "$HOME/cfg/bash/include/wttr.in" ] || ln -s "$HOME/cfg/custom/wttr.in/bashrc" "$HOME/cfg/bash/include/wttr.in"
 
