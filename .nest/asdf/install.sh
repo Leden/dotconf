850b9ce65672d2e8f127c134f0d7cbd61e5f743a
Denis Lesnov <denis@lesnov.me>
2019-09-17 01:20:57 +0300

    mv asdf installer to nest; bashrc

--- .nest/asdf/install.sh
+++ .nest/asdf/install.sh
@@ -4,9 +4,6 @@ set -xe
 
 [ -d "$HOME/.asdf" ] || git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.7.3
 
-mkdir -p "$HOME/cfg/bash/include/"
-[ -L "$HOME/cfg/bash/include/asdf" ] || ln -s "$HOME/cfg/custom/asdf/bashrc" "$HOME/cfg/bash/include/asdf"
-
 sudo apt install -y automake autoconf libreadline-dev \
   libncurses-dev libssl-dev libyaml-dev \
   libxslt-dev libffi-dev libtool unixodbc-dev \
