965d6ec5fec9b20ea7f0aa514d0a8e2da1b1af33
Denis Lesnov <denis@lesnov.me>
2020-09-04 14:12:55 +0300

    install gh, GitHub cli

---
+++ .nest/gh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+VERSION="0.11.1"
+OS="linux"
+ARCH="amd64"
+DEB_FILE="gh_${VERSION}_${OS}_${ARCH}.deb"
+URL="https://github.com/cli/cli/releases/download/v${VERSION}/${DEB_FILE}"
+
+
+wget "$URL" -O /tmp/"$DEB_FILE"
+
+sudo apt install -y /tmp/"$DEB_FILE"
+
+rm /tmp/"$DEB_FILE"
