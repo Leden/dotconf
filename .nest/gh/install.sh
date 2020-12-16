4f7ec831d843d602de53e6484c5f84877783538d
Denis Lesnov <denis@lesnov.me>
2020-12-16 15:46:19 +0200

    install latest gh version

--- .nest/gh/install.sh
+++ .nest/gh/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-VERSION="1.1.0"
+TAG="$(curl 'https://api.github.com/repos/cli/cli/releases/latest' | jq -r '.tag_name')"
+VERSION="${TAG##v}"
 OS="linux"
 ARCH="amd64"
 DEB_FILE="gh_${VERSION}_${OS}_${ARCH}.deb"
-URL="https://github.com/cli/cli/releases/download/v${VERSION}/${DEB_FILE}"
+URL="https://github.com/cli/cli/releases/download/${TAG}/${DEB_FILE}"
 
 
 wget "$URL" -O /tmp/"$DEB_FILE"
