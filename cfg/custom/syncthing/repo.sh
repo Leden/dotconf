c67fd139f715f2bdd5524658fc8c04f29d863846
Denis Lesnov <denis@lesnov.me>
2024-09-06 19:34:02 +0300

    move syncthing to nix home-manager

--- cfg/custom/syncthing/repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
